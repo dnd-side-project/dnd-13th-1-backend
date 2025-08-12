404351b120a53de3d7877d7ba0434d7dd5148634
yonghyeonpark <llyyoo93@gmail.com>
2025-08-13 04:10:16 +0900

    feat: houseWorkId 목록으로 HouseWorkMember 목록을 조회하는 기능 구현

---
+++ src/main/java/team1/housework/group/repository/HouseWorkMemberCustomRepositoryImpl.java
@@ -0,0 +1,23 @@
+package team1.housework.group.repository;
+
+import static team1.housework.group.entity.QHouseWorkMember.*;
+
+import java.util.List;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+import team1.housework.group.entity.HouseWorkMember;
+
+@RequiredArgsConstructor
+public class HouseWorkMemberCustomRepositoryImpl implements HouseWorkMemberCustomRepository {
+
+	private final JPAQueryFactory queryFactory;
+
+	@Override
+	public List<HouseWorkMember> findByHouseWorkIds(List<Long> houseWorkIds) {
+		return queryFactory.selectFrom(houseWorkMember)
+			.where(houseWorkMember.houseWork.id.in(houseWorkIds))
+			.fetch();
+	}
+}
--- src/main/java/team1/housework/group/repository/HouseWorkMemberRepository.java
+++ src/main/java/team1/housework/group/repository/HouseWorkMemberRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import team1.housework.group.entity.HouseWorkMember;
 
-public interface HouseWorkMemberRepository extends JpaRepository<HouseWorkMember, Long> {
+public interface HouseWorkMemberRepository
+	extends JpaRepository<HouseWorkMember, Long>, HouseWorkMemberCustomRepository {
 }
