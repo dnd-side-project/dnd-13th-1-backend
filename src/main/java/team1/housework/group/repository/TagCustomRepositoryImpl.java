a590a73067d542640fc8415382e2b54f7acb3047
yonghyeonpark <llyyoo93@gmail.com>
2025-08-13 04:13:00 +0900

    feat: id 목록으로 Tag 목록을 조회하는 기능 구현

---
+++ src/main/java/team1/housework/group/repository/TagCustomRepositoryImpl.java
@@ -0,0 +1,23 @@
+package team1.housework.group.repository;
+
+import static team1.housework.group.entity.QTag.*;
+
+import java.util.List;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+import team1.housework.group.entity.Tag;
+
+@RequiredArgsConstructor
+public class TagCustomRepositoryImpl implements TagCustomRepository {
+
+	private final JPAQueryFactory queryFactory;
+
+	@Override
+	public List<Tag> findByIds(List<Long> ids) {
+		return queryFactory.selectFrom(tag)
+			.where(tag.id.in(ids))
+			.fetch();
+	}
+}
--- src/main/java/team1/housework/group/repository/TagRepository.java
+++ src/main/java/team1/housework/group/repository/TagRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import team1.housework.group.entity.Tag;
 
-public interface TagRepository extends JpaRepository<Tag, Long> {
+public interface TagRepository extends JpaRepository<Tag, Long>, TagCustomRepository {
 
 	List<Tag> findByGroupId(Long groupId);
 }
