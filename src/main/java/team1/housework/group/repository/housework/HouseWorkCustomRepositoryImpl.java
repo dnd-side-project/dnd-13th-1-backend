00f71d990529888a14125a2049e1ee156248fead
yonghyeonpark <llyyoo93@gmail.com>
2025-08-14 06:43:43 +0900

    feat: 최근 7일 이내에 완료된 집안일들을 receiverId와 groupId로 필터링하여 조회하는 기능 구현

--- src/main/java/team1/housework/group/repository/housework/HouseWorkCustomRepositoryImpl.java
+++ src/main/java/team1/housework/group/repository/housework/HouseWorkCustomRepositoryImpl.java
@@ -1,6 +1,7 @@
 package team1.housework.group.repository.housework;
 
 import static team1.housework.group.entity.QHouseWork.*;
+import static team1.housework.group.entity.QHouseWorkMember.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
+import team1.housework.group.entity.HouseWork;
 
 @RequiredArgsConstructor
 public class HouseWorkCustomRepositoryImpl implements HouseWorkCustomRepository {
@@ -22,6 +24,22 @@ public class HouseWorkCustomRepositoryImpl implements HouseWorkCustomRepository 
 				houseWork.taskDate.between(from, to),
 				houseWork.group.id.eq(groupId)
 			)
+			.fetch();
+	}
+
+	@Override
+	public List<HouseWork> findHouseWorkRecent(Long groupId, Long receiverId, LocalDate currentDate) {
+		LocalDate weekAgo = currentDate.minusDays(6);
+
+		return queryFactory.selectFrom(houseWork)
+			.join(houseWorkMember).on(houseWork.id.eq(houseWorkMember.houseWork.id))
+			.join(houseWork.place).fetchJoin()
+			.where(
+				houseWork.group.id.eq(groupId),
+				houseWorkMember.memberId.eq(receiverId),
+				houseWork.completedDate.between(weekAgo, currentDate)
+			)
+			.orderBy(houseWork.completedDate.desc())
 			.fetch();
 	}
 }
