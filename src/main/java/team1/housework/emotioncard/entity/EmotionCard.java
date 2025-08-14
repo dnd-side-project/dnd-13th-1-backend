2d32eca4b207a2e194c9ecdad973917f972b830c
yonghyeonpark <llyyoo93@gmail.com>
2025-08-14 17:16:07 +0900

    refactor: Compliment를 엔티티로 분리

--- src/main/java/team1/housework/emotioncard/entity/EmotionCard.java
+++ src/main/java/team1/housework/emotioncard/entity/EmotionCard.java
@@ -2,19 +2,11 @@ package team1.housework.emotioncard.entity;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
 
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OrderColumn;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -36,29 +28,18 @@ public class EmotionCard {
 
 	private String disappointment;
 
-	@ElementCollection(fetch = FetchType.LAZY)
-	@CollectionTable(
-		name = "emotion_card_compliment",
-		joinColumns = @JoinColumn(name = "emotion_card_id")
-	)
-	@Column(name = "compliment")
-	@OrderColumn(name = "compliment_order")
-	private List<String> compliments = new ArrayList<>();
-
 	private LocalDateTime createdDate;
 
 	public EmotionCard(
 		Long senderId,
 		Long receiverId,
 		Long houseWorkId,
-		String disappointment,
-		List<String> compliments
+		String disappointment
 	) {
 		this.senderId = senderId;
 		this.receiverId = receiverId;
 		this.houseWorkId = houseWorkId;
 		this.disappointment = disappointment;
-		this.compliments = compliments;
 		this.createdDate = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
 	}
 }
