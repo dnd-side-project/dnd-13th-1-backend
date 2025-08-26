407ee922f65ffb72f194ebb1b34c6f511d035fe0
yonghyeonpark <llyyoo93@gmail.com>
2025-08-26 20:58:11 +0900

    fix: S3 관련 클래스들 주석 해제

--- src/main/java/team1/allo/storage/service/StorageService.java
+++ src/main/java/team1/allo/storage/service/StorageService.java
@@ -1,10 +1,10 @@
-// package team1.allo.storage.service;
-//
-// import org.springframework.web.multipart.MultipartFile;
-//
-// public interface StorageService {
-//
-// 	String uploadImageAndGetUrl(MultipartFile image);
-//
-// 	void deleteImage(String url);
-// }
+package team1.allo.storage.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+
+	String uploadImageAndGetUrl(MultipartFile image);
+
+	void deleteImage(String url);
+}
