91623402800066a580d445603df8dfc4692f1bdd
yonghyeonpark <llyyoo93@gmail.com>
2025-08-28 22:09:18 +0900

    hotfix: 에러 응답 코드를 400으로 수정

--- src/main/java/team1/allo/exception/GlobalExceptionHandler.java
+++ src/main/java/team1/allo/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package team1.allo.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import team1.allo.exception.dto.ErrorResponse;
@@ -8,6 +10,7 @@ import team1.allo.exception.dto.ErrorResponse;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler
 	public ErrorResponse exceptionHandler(RuntimeException e) {
 		return new ErrorResponse(e.getMessage());
